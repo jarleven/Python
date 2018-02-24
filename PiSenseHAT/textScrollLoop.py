5ef96ee546aa499743227e186ef7e10bf0531a45
Jarl Even Englund <jarleven@gmail.com>
2018-02-24 23:09:54 +0100

    Cleanup

---
+++ PiSenseHAT/textScrollLoop.py
@@ -0,0 +1,20 @@
+from sense_hat import SenseHat
+
+sense = SenseHat()
+
+red   = (255,   0,   0)
+green = (  0, 255,   0)
+blue  = (  0,   0, 255)
+
+while True:
+    sense.set_rotation(0)
+    sense.show_message("Hello world!")
+
+    sense.set_rotation(180)
+    sense.show_message("One small step for Pi!", text_colour=red)
+
+    sense.set_rotation(90)
+    sense.show_message("Hallo Verden!!", text_colour=green)
+
+    sense.set_rotation(270)
+    sense.show_message("Valfag", text_colour=blue

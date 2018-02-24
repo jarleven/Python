5cc8b78f00af9ea788dcd755b8302aa66e311a55
Jarl Even Englund <jarleven@gmail.com>
2018-02-24 23:25:07 +0100

    Claenup

--- PiSenseHAT/textscroll.py
+++ PiSenseHAT/textscroll.py
@@ -7,9 +7,9 @@ sense.show_message("Hello world!")
 sense = SenseHat()
 sense.set_rotation(180)
 
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0,0,255)
+red   = (255,   0,   0)
+green = (  0, 255,   0)
+blue  = (  0,   0, 255)
 
 sense.show_message("One small step for Pi!", text_colour=red)
 
@@ -19,27 +19,3 @@ sense.show_message("Hallo Verden!!", text_colour=green)
 sense.set_rotation(270)
 sense.show_message("Valfag", text_colour=blue)
 
-
-***************
-
-
-from sense_hat import SenseHat
-
-sense = SenseHat()
-
-red = (255, 0, 0)
-green = (0, 255, 0)
-blue = (0,0,255)
-
-while True:
-    sense.set_rotation(0)
-    sense.show_message("Hello world!")
-
-    sense.set_rotation(180)
-    sense.show_message("One small step for Pi!", text_colour=red)
-
-    sense.set_rotation(90)
-    sense.show_message("Hallo Verden!!", text_colour=green)
-
-    sense.set_rotation(270)
-    sense.show_message("Valfag", text_colour=blue

83344f7a2f3790bc63c0e7d56153dc110020c4af
jarleven <jarleven@gmail.com>
2018-02-28 14:52:40 +0100

    Update bondesjakk-brett.py
    Use formated text

--- bondesjakk-brett.py
+++ bondesjakk-brett.py
@@ -54,15 +54,15 @@ ruter =    [0, "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 def brett(board):
     print(' ')
     print('   |   |')
-    print(' ' + board[7] + ' | ' + board[8] + ' | ' + board[9])
+    print(' %s | %s | %s' % (board[7],board[8],board[9]))
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[4] + ' | ' + board[5] + ' | ' + board[6])
+    print(' %s | %s | %s' % (board[4],board[5],board[6]))
     print('   |   |')
     print('-----------')
     print('   |   |')
-    print(' ' + board[1] + ' | ' + board[2] + ' | ' + board[3])
+    print(' %s | %s | %s' % (board[1],board[2],board[3]))
     print('   |   |')
     print(' ')
     

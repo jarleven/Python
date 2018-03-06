e68cea661e8a960a824f27a4658d158691ec92cc
Jarl Even Englund <jarleven@gmail.com>
2018-03-06 16:43:48 +0100

    Moved firstGUI

---
+++ QTDesigner/firstgui/firstgui.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'firstgui.ui'
+#
+# Created by: PyQt5 UI code generator 5.9
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+class Ui_myfirstgui(object):
+    def setupUi(self, myfirstgui):
+        myfirstgui.setObjectName("myfirstgui")
+        myfirstgui.resize(170, 238)
+        self.buttonBox = QtWidgets.QDialogButtonBox(myfirstgui)
+        self.buttonBox.setGeometry(QtCore.QRect(-230, 190, 381, 32))
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Close)
+        self.buttonBox.setObjectName("buttonBox")
+        self.dial = QtWidgets.QDial(myfirstgui)
+        self.dial.setGeometry(QtCore.QRect(10, 10, 121, 131))
+        self.dial.setNotchesVisible(True)
+        self.dial.setObjectName("dial")
+        self.progressBar = QtWidgets.QProgressBar(myfirstgui)
+        self.progressBar.setGeometry(QtCore.QRect(20, 150, 141, 23))
+        self.progressBar.setProperty("value", 0)
+        self.progressBar.setObjectName("progressBar")
+        self.lcdNumber = QtWidgets.QLCDNumber(myfirstgui)
+        self.lcdNumber.setGeometry(QtCore.QRect(120, 120, 31, 23))
+        self.lcdNumber.setDigitCount(2)
+        self.lcdNumber.setObjectName("lcdNumber")
+        self.label = QtWidgets.QLabel(myfirstgui)
+        self.label.setGeometry(QtCore.QRect(60, 150, 71, 21))
+        self.label.setObjectName("label")
+
+        self.retranslateUi(myfirstgui)
+        self.buttonBox.accepted.connect(myfirstgui.accept)
+        self.buttonBox.rejected.connect(myfirstgui.reject)
+        QtCore.QMetaObject.connectSlotsByName(myfirstgui)
+
+    def retranslateUi(self, myfirstgui):
+        _translate = QtCore.QCoreApplication.translate
+        myfirstgui.setWindowTitle(_translate("myfirstgui", "My First Gui!"))
+        self.label.setText(_translate("myfirstgui", "Normal"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    myfirstgui = QtWidgets.QDialog()
+    ui = Ui_myfirstgui()
+    ui.setupUi(myfirstgui)
+    myfirstgui.show()
+    sys.exit(app.exec_())
+

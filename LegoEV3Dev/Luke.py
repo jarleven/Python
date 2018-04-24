083cc82974b05af2ac05ecc2aa86fdee48234ed5
Jarl Even Englund <jarleven@gmail.com>
2018-04-24 16:21:54 +0200

    Initial EV3 Dev work

--- LegoEV3Dev/Luke.py
+++ LegoEV3Dev/Luke.py
@@ -1,9 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr 24 10:56:47 2018
+
+@author: jareng
+"""
+
 import rpyc
+
 
 # Remote Python Call   RPyC (pronounced are-pie-see)
 
-conn = rpyc.classic.connect('192.168.137.3')    # host name or IP address of the EV3
+conn = rpyc.classic.connect('192.168.137.3')   # host name or IP address of the EV3
 ev3 = conn.modules['ev3dev.ev3']                # import ev3dev.ev3 remotely
 
-ev3.Sound.speak('Luke Im your father').wait()
-
+ev3.Sound.speak("Luke I'm your father").wait()
--- LegoEV3Dev/README.md
+++ LegoEV3Dev/README.md
@@ -1,10 +1,7 @@
 
+http://ev3dev-lang.readthedocs.io/projects/python-ev3dev/en/stable/rpyc.html
 
 
-import rpyc
+https://sites.google.com/site/ev3python/learn_ev3_python
 
-conn = rpyc.classic.connect('192.168.137.3')   # host name or IP address of the EV3
-ev3 = conn.modules['ev3dev.ev3']                # import ev3dev.ev3 remotely
-
-ev3.Sound.speak('Luke Im your father').wait()
 

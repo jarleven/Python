bd2ae79cfb04a6b4c67e8e861a678a8e1c461b1e
Jarl Even Englund <jarleven@gmail.com>
2018-04-24 17:13:37 +0200

    Added examples of more motor classes

--- LegoEV3Dev/Luke.py
+++ LegoEV3Dev/Luke.py
@@ -11,6 +11,7 @@ import rpyc
 # Remote Python Call   RPyC (pronounced are-pie-see)
 
 conn = rpyc.classic.connect('192.168.137.3')   # host name or IP address of the EV3
-ev3 = conn.modules['ev3dev.ev3']                # import ev3dev.ev3 remotely
+ev3 = conn.modules['ev3dev.ev3']               # import ev3dev.ev3 remotely
+
 
 ev3.Sound.speak("Luke I'm your father").wait()

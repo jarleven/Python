82fa3588fd13d8fd5a30da271ab12842820d5ea4
Jarl Even Englund <jarleven@gmail.com>
2018-04-24 18:12:29 +0200

    More sensors

--- LegoEV3Dev/GyroSensor.py
+++ LegoEV3Dev/GyroSensor.py
@@ -1,19 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Apr 24 16:15:10 2018
+Created on Tue Apr 24 18:04:19 2018
 
 @author: jareng
 """
 
 import rpyc
 
-
 # Remote Python Call  RPyC (pronounced are-pie-see)
 
 conn = rpyc.classic.connect('192.168.137.3')   # host name or IP address of the EV3
 ev3 = conn.modules['ev3dev.ev3']               # import ev3dev.ev3 remotely
 
-us = ev3.UltrasonicSensor('in1')
+gs = ev3.GyroSensor("in1")
 
-print("Avstanden er %f" % us.distance_centimeters)
-    
+print(gs.angle)
+
+
+"""
+
+    print(gs.angle)
+ 
+    print(gs.rate)
+
+    Note to reset the angle call the rate one time
+
+"""
--- LegoEV3Dev/TouchSensor.py
+++ LegoEV3Dev/TouchSensor.py
@@ -7,13 +7,28 @@ Created on Tue Apr 24 17:08:42 2018
 
 import rpyc
 
-
 # Remote Python Call  RPyC (pronounced are-pie-see)
 
 conn = rpyc.classic.connect('192.168.137.3')   # host name or IP address of the EV3
 ev3 = conn.modules['ev3dev.ev3']               # import ev3dev.ev3 remotely
 
-mD = ev3.MediumMotor('outD')
-mD.run_timed(time_sp=1000, speed_sp=1000)
-mD.wait_until_not_moving()
-mD.run_timed(time_sp=1000, speed_sp=-1000)
+ts1 = ev3.TouchSensor("in1")
+ts2 = ev3.TouchSensor("in2")
+
+ev3.Leds.set_color(ev3.Leds.LEFT,  ev3.Leds.YELLOW)
+ev3.Leds.set_color(ev3.Leds.RIGHT, ev3.Leds.YELLOW)
+
+# Colors can be RED, GREEN, YELLOW, ORANGE or AMBER.
+
+
+while True:
+
+    if ts1.value():
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.RED)
+    else:
+        ev3.Leds.set_color(ev3.Leds.LEFT, ev3.Leds.GREEN)
+    
+    if ts2.value():
+        break
+    
+ev3.Sound.speak("Goodbye").wait()

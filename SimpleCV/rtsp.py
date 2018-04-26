211f1393b0a5b2c3b7bf28bc4c9b22825ade4ce2
Jarl Even Englund <jarleven@gmail.com>
2018-04-26 22:26:07 +0200

    Fixed bad whitespace in file

--- SimpleCV/rtsp.py
+++ SimpleCV/rtsp.py
@@ -6,16 +6,19 @@
 """
 
 import cv2
- 
-camera = cv2.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp")
- 
+
+#camera = cv2.VideoCapture("rtsp://192.168.1.10:554/user=admin&password=&channel=1&stream=0.sdp")
+camera = cv2.VideoCapture("rtsp://192.168.1.88:554/user=admin&password=&channel=1&stream=0.sdp")
+
+
+
 while(1):
-    ret, frame = camera.read()
-    cv2.imshow('VIDEO', frame)
-    cv2.waitKey(1)
- 
-    key = cv2.waitKey(1) & 0xFF
- 
-    # if the `q` key is pressed, break from the lop
-    if key == ord("q"):
-        break
+    ret, frame = camera.read()
+    cv2.imshow('VIDEO', frame)
+    cv2.waitKey(1)
+    
+    key = cv2.waitKey(1) & 0xFF
+    
+    # if the `q` key is pressed, break from the lop
+    if key == ord("q"):
+        break

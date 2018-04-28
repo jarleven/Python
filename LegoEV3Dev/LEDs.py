ef62f18a7d37c99c3d5a404eea69becdeb31068f
Jarl Even Englund <jarleven@gmail.com>
2018-04-28 19:00:57 +0200

    New example with LEDs

---
+++ LegoEV3Dev/LEDs.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr 28 18:48:19 2018
+
+@author: jareng
+"""
+
+import rpyc
+
+
+# Remote Python Call   RPyC (pronounced are-pie-see)
+
+conn = rpyc.classic.connect('192.168.1.135')   # host name or IP address of the EV3
+ev3 = conn.modules['ev3dev.ev3']               # import ev3dev.ev3 remotely
+
+
+ev3.Leds.set_color(ev3.Leds.LEFT,  ev3.Leds.YELLOW)
+
+# Code below will loop all the colors available on both LEDs of the EV3
+
+
+# Colors can be RED, GREEN, YELLOW, ORANGE or AMBER.
+colors=[ev3.Leds.RED, ev3.Leds.GREEN, ev3.Leds.YELLOW, ev3.Leds.ORANGE, ev3.Leds.AMBER]
+
+while True:
+    for color in colors:
+        ev3.Leds.set_color(ev3.Leds.LEFT,  color)
+        ev3.Leds.set_color(ev3.Leds.RIGHT, color)
+

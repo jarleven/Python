9966cc3aa352c3ff78eacfb806aca9955686e2ef
jarleven <jarleven@gmail.com>
2018-11-07 19:45:25 +0100

    Update Square.py
    Fixed a few things in todays class

--- UniversaRobots/Square.py
+++ UniversaRobots/Square.py
@@ -1,13 +1,15 @@
 import urx
-import time
 
 rob = urx.Robot("192.168.1.114")
 
-Movements are in centimeters as we divide by 100 in the movel commands 
+#Movements are in centimeters as we divide by 100 in the movel commands 
+
+# movel((x,y,z,rx,ry,rz, acceleration, velocity, relative=True, wait=True)
 
 # Pen down
-z=-0.48
-rob.movel((0, 0, z/100, 0, 0, 0), 0.01, 0.05, relative=True)  # move relative to current pose
+z=-0.7
+zmove=z/100
+rob.movel((0, 0, zmove, 0, 0, 0), 0.01, 0.05, relative=True, wait=True))  # move relative to current pose
     
 # The X/Y dimmentions of the rectangle we will draw
 x=[-10,  0, 10, 0]
@@ -15,9 +17,12 @@ y=[ 0,  -3, 0,  3]
 
 # Draw the rectangle in the X,Y plane
 for i in range(0,4):
-    rob.movel((x[i]/100, y[i]/100, 0, 0, 0, 0), 0.01, 0.05, relative=True, wait=True)  # move relative to current pose
+    xmove=x[i]/100
+    ymove=y[i]/100
+    rob.movel((xmove, ymove, 0, 0, 0, 0), 0.01, 0.05, relative=True, wait=True)  # move relative to current pose
 
 # Pen up
-z=.48
-rob.movel((0, 0, z/100, 0, 0, 0), 0.01, 0.05, relative=True)  # move relative to current pose
+z=.7
+zmove=z/100
+rob.movel((0, 0, zmove, 0, 0, 0), 0.01, 0.05, relative=True, wait=True))  # move relative to current pose
         

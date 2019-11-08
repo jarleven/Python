bd10b88fc0fff6bc551747df97aebbe2baa6316a
jarleven <jarleven@gmail.com>
2019-11-08 20:55:32 +0100

    Update HelloWorld.py
    Simple as it gets

--- PiGPIO/HelloWorld.py
+++ PiGPIO/HelloWorld.py
@@ -1,2 +1,39 @@
 # As simple as it gets
 
+#
+# Import Libraries
+#
+
+import time
+import RPi.GPIO as GPIO # import our GPIO library
+
+#
+# Define our stuff
+#
+
+# The pin we blink on the Raspberry Pi, and the delay in seconds
+GPIOPIN = 4
+# Time in seconds for LED states
+BLINKTIME = 1
+
+
+#
+# Code 
+#
+
+GPIO.setmode(GPIO.BCM) # set the board numbering system to BCM
+
+GPIO.setup(GPIOPIN,GPIO.OUT)
+#GPIO.setup(pin,GPIO.IN)
+
+
+
+# Blink the pin
+print("Pi light'em up!")
+
+# Just go roundabout here blinking the LEDs
+while True:
+    GPIO.output(pin,GPIO.HIGH)
+    time.sleep(BLINKTIME)
+    GPIO.output(pin,GPIO.LOW)
+    time.sleep(BLINKTIME)

d14a585156138331b89b0fc03ed05d6ee500c58e
Jarl Even Englund <jarleven@gmail.com>
2019-12-28 09:00:46 -0800

    An IoT project

---
+++ MicroPython/IoT-button/boot.py
@@ -0,0 +1,40 @@
+# This file is executed on every boot (including wake-boot from deepsleep)
+import myConfig
+import network
+import time
+from machine import WDT
+
+
+time.sleep(1)
+print("")
+print("Booting...")
+
+sta_if = network.WLAN(network.STA_IF)
+ap_if = network.WLAN(network.AP_IF)
+
+
+def startWebrepl():
+    import webrepl
+    webrepl.start()
+
+
+def do_connect():
+
+    if not sta_if.isconnected():
+        print('connecting to network...')
+        sta_if.active(True)
+        sta_if.connect(myConfig.AP, myConfig.KEY)
+        while not sta_if.isconnected():
+            wdt.feed()
+            pass
+    print('network config:', sta_if.ifconfig())
+
+
+print('disable AP')
+ap_if.active(False)
+
+wdt = WDT()  # enable watchdog timer
+wdt.feed()
+do_connect()
+wdt.feed()
+
---
+++ MicroPython/IoT-button/main.py
@@ -0,0 +1,196 @@
+# Set internal LED with the FLASH pushbutton. Interrupt controlled
+from machine import Pin
+from machine import Timer
+from machine import reset
+import socket
+import boot
+import ubinascii
+
+#from uptime import uptime
+import utime
+import time
+
+
+PRELLTIME=100
+POLLTIME=100
+IGNORETIME=100
+
+
+pins=myConfig.pins
+led=myConfig.LEDPIN
+
+HOST, PORT=(myConfig.HOST, myConfig.PORT)
+
+espmac = ubinascii.hexlify(network.WLAN().config('mac'),':').decode()   
+
+if espmac in myConfig.module:
+    print("Found MAC %s" % espmac)
+    lamp = myConfig.module[espmac]
+else:
+    print("Unknown device MAC %s" % espmac)
+    lamp = [10, 6, 7, 8]
+
+print("IO pins are : ", pins)
+print("LAMPS are   : ", lamp)
+
+
+onboardLED = Pin(led, Pin.OUT)
+onboardLED.off()
+
+
+
+button = [None] * 4
+tim = [None] *4
+SendState = [None] *4
+
+
+def isr_one(p):
+    isr_common(0)
+
+def isr_two(p):
+    isr_common(1)
+
+def isr_three(p):
+    isr_common(2)
+
+def isr_four(p):
+    isr_common(3)
+
+
+def timer_cb_one(t):
+    timer_cb_common(0)
+
+def timer_cb_two(t):
+    timer_cb_common(1)
+
+def timer_cb_three(t):
+    timer_cb_common(2)
+
+def timer_cb_four(t):
+    timer_cb_common(3)
+
+
+isr_func = [isr_one, isr_two, isr_three, isr_four]
+timer_cb = [timer_cb_one,timer_cb_two,timer_cb_three, timer_cb_four]
+
+
+
+for idx in range(4):
+    SendState[idx] = 0 # 0 idle, 1 ISR, 2 Steadey
+    button[idx] = Pin(pins[idx], Pin.IN, Pin.PULL_UP)
+    tim[idx] = Timer(-1)
+
+
+
+# For the ESP8266 there is some chaos regarding the pin interrupt handling
+# Quote Mar 17, 2018 "p.irq(trigger=0,handler=callback) seems to properly disable the interruptions."
+# https://github.com/micropython/micropython/issues/2847
+
+def enable_btirq(idx):
+    button[idx].irq(trigger=Pin.IRQ_FALLING, handler=isr_func[idx])
+    button[idx] = Pin(pins[idx], Pin.IN, Pin.PULL_UP)
+
+def disable_btirq(idx):
+    button[idx].irq(trigger=0, handler=isr_func[idx])
+
+
+
+def isr_common(idx):
+    global SendState              # To modify a global variable we need to state it's name
+    SendState[idx] = 1
+    disable_btirq(idx)
+      
+
+def timer_cb_common(idx):
+
+    if SendState[idx] == 2:
+        if button[idx].value():
+            SendState[idx] = 0
+            enable_btirq(idx)
+            print("Ignored glitch press %d, waiting for new user input" % idx)
+        else:
+            print("Captured button press %d" % idx)
+            SendState[idx] = 3
+
+    else:
+
+        if not button[idx].value():
+            tim[idx].init(period=PRELLTIME, mode=Timer.ONE_SHOT, callback=timer_cb[idx])
+            print("Button %d not released" % idx)
+        else:
+            enable_btirq(idx)
+            print("Ready button %d, waiting for new user input" % idx)
+
+
+# Send the data over the network
+def sendData(netData):
+    print("Sending data: ")
+    print(netData)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((HOST, PORT))
+    sock.sendall(netData + "\n\n")
+    received = sock.recv(1024)
+    sock.close()
+
+    print("Got data echo: ")
+    print(received)
+
+
+
+
+# Register interrup pin
+for idx in range(4):
+    enable_btirq(idx)
+    print("Enabling button %d" % idx)
+
+
+# Loop
+while True:
+
+    wdt.feed()
+
+
+    if(boot.sta_if.isconnected()):
+        onboardLED.off()
+    else:
+        onboardLED.on()
+        
+
+    if( utime.ticks_ms() > 1000 * 60 * 60 * 24 ):
+        print("Reboot now")
+        reset()
+
+
+    for idx in range(4):
+        if SendState[idx] == 1:
+            # Clear the new event flag set in the interrupt
+            print("Trigger, wait a bit")
+            SendState[idx] = 2
+
+
+            # We have an event and the interrupt was disabled. Enable the interruptagain with the timer
+            # This is our debounce
+            tim[idx].init(period=IGNORETIME, mode=Timer.ONE_SHOT, callback=timer_cb[idx])
+            # Timer example from https://www.dfrobot.com/blog-606.html
+
+        if SendState[idx] == 3:
+
+            SendState[idx] = 0
+            tim[idx].init(period=POLLTIME, mode=Timer.ONE_SHOT, callback=timer_cb[idx])
+
+            if(idx==0):
+                for a in range(1, 4):
+                    print("ID %d  state %d" % (a, SendState[a]))              
+                    enable_btirq(a)
+                    boot.startWebrepl()
+
+
+            else:
+
+                netValue = lamp[idx]
+                netData = "binary LED%d toggle" % (netValue)
+
+                sendData(netData)
+                print("Action on LED%d" % lamp[idx])
+
+

0249892fec38f79541411490607073fd568eeb62
Jarl Even Englund <jarleven@gmail.com>
2021-02-06 08:13:36 -0800

    Install script

---
+++ LED-Matrix/luma/install-luma.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+
+
+sudo apt install -y python3-dev python3-pip libfreetype6-dev libjpeg-dev build-essential
+sudo apt install -y libsdl-dev libportmidi-dev libsdl-ttf2.0-dev libsdl-mixer1.2-dev libsdl-image1.2-dev
+sudo apt install -y python3-testresources
+sudo apt install -y python3-dev python3-pip build-essential
+
+sudo -H pip3 install --upgrade --ignore-installed pip setuptools
+
+
+cd ~
+git clone https://github.com/rm-hull/luma.examples.git
+git clone https://github.com/rm-hull/luma.core.git
+git clone https://github.com/rm-hull/luma.emulator.git
+
+
+cd ~/luma.core
+sudo -H pip3 install -e .
+
+cd ~/luma.examples
+sudo -H pip3 install -e .
+
+cd ~/luma.emulator
+sudo -H pip3 install -e .
+
+
+cd ~/luma.examples
+# python3 examples/clock.py --display capture
+# python3 examples/clock.py --display pygame
+python3 examples/matrix.py --display pygame --transform=led_matrix --width 40 --height 16

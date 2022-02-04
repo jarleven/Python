b2f01b2307666ccd0e8556ad858f9b61082e0bf2
jarleven <jarleven@gmail.com>
2022-02-04 23:22:52 +0100

    Update setup.sh
    Load my own version of the image example

--- ILI9340/setup.sh
+++ ILI9340/setup.sh
@@ -43,36 +43,36 @@ cd Adafruit_Python_ILI9340/examples
 
 python3 image.py
 
+wget https://raw.githubusercontent.com/jarleven/Python/master/ILI9340/image.py
 
-
- # wget https://images.hdqwalls.com/walls/thumb/pokemon-detective-pikachu-poster-5k-lc.jpg -O pikachu.jpg
- # wget https://images.hdqwalls.com/walls/thumb/ninetales-one-more-pokemon-pu.jpg -O ninetales.jpg
+ wget https://images.hdqwalls.com/walls/thumb/pokemon-detective-pikachu-poster-5k-lc.jpg -O pikachu.jpg
+ wget https://images.hdqwalls.com/walls/thumb/ninetales-one-more-pokemon-pu.jpg -O ninetales.jpg
 
 
 # Connecting as instructed in this example : https://github.com/Funkrusha/Adafruit_Python_ILI9340/blob/master/examples/image.py
 : '
 ```Bash
 
-           3V3  (1) (2)  5V		    VCC
-         GPIO2  (3) (4)  5V			  
-         GPIO3  (5) (6)  GND		  GND
-         GPIO4  (7) (8)  GPIO14
-           GND  (9) (10) GPIO15
-        GPIO17 (11) (12) GPIO18
-        GPIO27 (13) (14) GND
-        GPIO22 (15) (16) GPIO23		RESET
-           3V3 (17) (18) GPIO24
+	           3V3  (1) (2)  5V		    VCC
+	         GPIO2  (3) (4)  5V			  
+	         GPIO3  (5) (6)  GND		  GND
+	         GPIO4  (7) (8)  GPIO14
+	           GND  (9) (10) GPIO15
+	        GPIO17 (11) (12) GPIO18
+	        GPIO27 (13) (14) GND
+	        GPIO22 (15) (16) GPIO23		RESET
+	           3V3 (17) (18) GPIO24
 MOSI		GPIO10 (19) (20) GND
 MISO		 GPIO9 (21) (22) GPIO25		D/C
-SCK		  GPIO11 (23) (24) GPIO8		LCD CS
-           GND (25) (26) GPIO7
-         GPIO0 (27) (28) GPIO1
-         GPIO5 (29) (30) GND
-			   GPIO6 (31) (32) GPIO12
-			  GPIO13 (33) (34) GND
-			  GPIO19 (35) (36) GPIO16
-			  GPIO26 (37) (38) GPIO20
-			     GND (39) (40) GPIO21
+SCK		GPIO11 (23) (24) GPIO8		LCD CS
+	           GND (25) (26) GPIO7
+	         GPIO0 (27) (28) GPIO1
+	         GPIO5 (29) (30) GND
+	   	 GPIO6 (31) (32) GPIO12
+		GPIO13 (33) (34) GND
+		GPIO19 (35) (36) GPIO16
+		GPIO26 (37) (38) GPIO20
+		   GND (39) (40) GPIO21
 
 ```
 '

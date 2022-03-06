fa2936da2a38f92ee84fe4671294945196ca3431
jarleven <jarleven@gmail.com>
2022-03-06 17:20:32 +0100

    Add files via upload

---
+++ LED-Matrix/luma/hack/matrix_demo.py
@@ -0,0 +1,124 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (c) 2017-18 Richard Hull and contributors
+# See LICENSE.rst for details.
+
+import re
+import time
+import argparse
+
+from luma.led_matrix.device import max7219
+from luma.core.interface.serial import spi, noop
+from luma.core.render import canvas
+from luma.core.virtual import viewport
+from luma.core.legacy import text, show_message
+from luma.core.legacy.font import proportional, CP437_FONT, TINY_FONT, SINCLAIR_FONT, LCD_FONT
+
+MY_MAPPING = [
+        63,  62,  61,  60,  59,  58,  57,  56,  64,  72,  80,  88,  96, 104, 112, 120, 
+        55,  54,  53,  52,  51,  50,  49,  48,  65,  73,  81,  89,  97, 105, 113, 121, 
+        47,  46,  45,  44,  43,  42,  41,  40,  66,  74,  82,  90,  98, 106, 114, 122, 
+        39,  38,  37,  36,  35,  34,  33,  32,  67,  75,  83,  91,  99, 107, 115, 123, 
+        31,  30,  29,  28,  27,  26,  25,  24,  68,  76,  84,  92, 100, 108, 116, 124, 
+        23,  22,  21,  20,  19,  18,  17,  16,  69,  77,  85,  93, 101, 109, 117, 125, 
+        15,  14,  13,  12,  11,  10,   9,   8,  70,  78,  86,  94, 102, 110, 118, 126, 
+         7,   6,   5,   4,   3,   2,   1,   0,  71,  79,  87,  95, 103, 111, 119, 127, 
+    ]
+
+
+def demo(n, block_orientation, rotate, inreverse):
+    # create matrix device
+    serial = spi(port=0, device=0, gpio=noop())
+    device = max7219(serial, width=16, height=8, mapping=MY_MAPPING)
+    print("Created device")
+
+    #time.sleep(2)
+    # start demo
+    msg = "MAX7219 LED Matrix Demo"
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(CP437_FONT))
+    time.sleep(1)
+
+    msg = "Slow scrolling: The quick brown fox jumps over the lazy dog"
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(LCD_FONT), scroll_delay=0.1)
+
+    print("Vertical scrolling")
+    words = [
+        "Victor", "Echo", "Romeo", "Tango", "India", "Charlie", "Alpha",
+        "Lima", " ", "Sierra", "Charlie", "Romeo", "Oscar", "Lima", "Lima",
+        "India", "November", "Golf", " "
+    ]
+
+    virtual = viewport(device, width=device.width, height=len(words) * 8)
+    with canvas(virtual) as draw:
+        for i, word in enumerate(words):
+            text(draw, (0, i * 8), word, fill="white", font=proportional(CP437_FONT))
+
+    for i in range(virtual.height - device.height):
+        virtual.set_position((0, i))
+        time.sleep(0.05)
+
+    msg = "Brightness"
+    print(msg)
+    show_message(device, msg, fill="white")
+
+    time.sleep(1)
+    with canvas(device) as draw:
+        text(draw, (0, 0), "A", fill="white")
+
+    time.sleep(1)
+    for _ in range(5):
+        for intensity in range(16):
+            device.contrast(intensity * 16)
+            time.sleep(0.1)
+
+    device.contrast(0x80)
+    time.sleep(1)
+
+    msg = "Alternative font!"
+    print(msg)
+    show_message(device, msg, fill="white", font=SINCLAIR_FONT)
+
+    time.sleep(1)
+    msg = "Proportional font - characters are squeezed together!"
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(SINCLAIR_FONT))
+
+    # http://www.squaregear.net/fonts/tiny.shtml
+    time.sleep(1)
+    msg = "Tiny is, I believe, the smallest possible font \
+    (in pixel size). It stands at a lofty four pixels \
+    tall (five if you count descenders), yet it still \
+    contains all the printable ASCII characters."
+    msg = re.sub(" +", " ", msg)
+    print(msg)
+    show_message(device, msg, fill="white", font=proportional(TINY_FONT))
+
+    time.sleep(1)
+    msg = "CP437 Characters"
+    print(msg)
+    show_message(device, msg)
+
+    time.sleep(1)
+    for x in range(256):
+        with canvas(device) as draw:
+            text(draw, (0, 0), chr(x), fill="white")
+            time.sleep(0.1)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='matrix_demo arguments',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--cascaded', '-n', type=int, default=1, help='Number of cascaded MAX7219 LED matrices')
+    parser.add_argument('--block-orientation', type=int, default=0, choices=[0, 90, -90], help='Corrects block orientation when wired vertically')
+    parser.add_argument('--rotate', type=int, default=0, choices=[0, 1, 2, 3], help='Rotate display 0=0°, 1=90°, 2=180°, 3=270°')
+    parser.add_argument('--reverse-order', type=bool, default=False, help='Set to true if blocks are in reverse order')
+
+    args = parser.parse_args()
+
+    try:
+        demo(args.cascaded, args.block_orientation, args.rotate, args.reverse_order)
+    except KeyboardInterrupt:
+        pass

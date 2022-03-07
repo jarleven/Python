eefffc56adee5497ce9f68a219f06244de303b64
jarleven <jarleven@gmail.com>
2022-03-07 22:32:55 +0100

    Update EventPushbutton.py

--- PiGPIO/EventPushbutton.py
+++ PiGPIO/EventPushbutton.py
@@ -5,13 +5,12 @@ import RPi.GPIO as GPIO
 def myCallback(channel):
     print("Button %s was pushed" % channel)
 
-print("Connect button to GPIO17/Pin11 and GND")
 GPIO.setmode(GPIO.BCM)
-
 GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-
-
 GPIO.add_event_detect(17, GPIO.FALLING, callback=myCallback, bouncetime=250)
+
+print("Connect button to GPIO17/Pin11 and GND")
+
 while True:
     pass
 

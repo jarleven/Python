671fe578086b2bad92c2ca507ee098ec9c58abdc
jarleven <jarleven@gmail.com>
2022-10-04 23:08:51 +0200

    Create ros-foxy.sh

---
+++ Micro-ROS/ros-foxy.sh
@@ -0,0 +1,16 @@
+# wget 
+
+# https://docs.ros.org/en/foxy/Installation/Ubuntu-Install-Debians.html
+
+sudo apt update && sudo apt install curl gnupg2 lsb-release
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key  -o /usr/share/keyrings/ros-archive-keyring.gpg
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(source /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+sudo apt update
+sudo apt upgrade -y
+
+sudo apt install -y ros-foxy-desktop
+
+
+

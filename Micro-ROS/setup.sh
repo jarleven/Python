b6e4186d44386944ff37d4b1f1bffa7fadbd2531
jarleven <jarleven@gmail.com>
2022-10-04 20:41:12 +0200

    Update setup.sh

--- Micro-ROS/setup.sh
+++ Micro-ROS/setup.sh
@@ -6,6 +6,8 @@
 
 sudo apt update && sudo apt upgrade -y
 
+# Step 1 — install pre-requisites
+
 sudo apt install -y build-essential cmake gcc-arm-none-eabi libnewlib-arm-none-eabi doxygen git python3
 
 # Configure environment variables
@@ -13,6 +15,8 @@ echo "export PICO_TOOLCHAIN_PATH=..." >> ~/.bashrc
 echo "export PICO_SDK_PATH=$HOME/micro_ros_ws/src/pico-sdk" >> ~/.bashrc
 source ~/.bashrc
 
+
+# Step 2 — Download the pico sdk code and micro-ROS example code from github repos
 
 mkdir -p ~/micro_ros_ws/src
 cd ~/micro_ros_ws/src
@@ -22,4 +26,11 @@ git clone --recurse-submodules https://github.com/raspberrypi/pico-sdk.git
 git clone https://github.com/micro-ROS/micro_ros_raspberrypi_pico_sdk.git
 
 
+# Step 3— Build the code that will generate a uf2 file that can be copied to Pico
+
+cd micro_ros_raspberrypi_pico_sdk
+mkdir build
+cd build
+cmake ..
+make
 

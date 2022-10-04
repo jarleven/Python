947409a512910055652af04daea55d97e313129c
jarleven <jarleven@gmail.com>
2022-10-04 21:58:34 +0200

    Update setup.sh

--- Micro-ROS/setup.sh
+++ Micro-ROS/setup.sh
@@ -1,60 +1,30 @@
 
 # wget https://raw.githubusercontent.com/jarleven/Python/master/Micro-ROS/setup.sh && chmod +x setup.sh
 
-# Thanks to https://robofoundry.medium.com/raspberry-pi-pico-ros2-via-micro-ros-actually-working-in-1-hr-9f7a3782d3e3
+sudo apt install cmake g++ gcc-arm-none-eabi doxygen libnewlib-arm-none-eabi git python3
 
 
+# which arm-none-eabi-gcc
 
+echo "export PICO_TOOLCHAIN_PATH=/usr/bin/arm-none-eabi-gcc" >> ~/.bashrc
+echo "export PICO_SDK_PATH=$HOME/pico-sdk" >> ~/.bashrc
 
-
-####  Final changes
-
-https://github.com/micro-ROS/micro_ros_raspberrypi_pico_sdk/blob/humble/CMakeLists.txt
-
-which arm-none-eabi-gcc
-/usr/bin/arm-none-eabi-gcc
+# Install Pico SDK
+git clone --recurse-submodules https://github.com/raspberrypi/pico-sdk.git $HOME/pico-sdk
 
 git clone https://github.com/micro-ROS/micro_ros_raspberrypi_pico_sdk.git
 
-####
-
-
-
-sudo apt update && sudo apt upgrade -y
-
-# Step 1 — install pre-requisites
-
-sudo apt install -y build-essential cmake gcc-arm-none-eabi libnewlib-arm-none-eabi doxygen git python3
-
-# Configure environment variables
-echo "export PICO_TOOLCHAIN_PATH=..." >> ~/.bashrc
-echo "export PICO_SDK_PATH=$HOME/micro_ros_ws/src/pico-sdk" >> ~/.bashrc
 source ~/.bashrc
 
-
-# Step 2 — Download the pico sdk code and micro-ROS example code from github repos
-
-mkdir -p ~/micro_ros_ws/src
-cd ~/micro_ros_ws/src
-### clone the pico-sdk
-git clone --recurse-submodules https://github.com/raspberrypi/pico-sdk.git
-### clone the micro-ros example repo. This example simply publishes an incrementing integer value every second
-git clone https://github.com/micro-ROS/micro_ros_raspberrypi_pico_sdk.git
-
-
-# Step 3— Build the code that will generate a uf2 file that can be copied to Pico
+# Once the Pico SDK is ready compile the example:
 
 cd micro_ros_raspberrypi_pico_sdk
 mkdir build
 cd build
-cmake .
+cmake ..
 make
 
+# Copy the uf2 file over to the RPi Pico (Remember to puch the BOOTSEL button)
+# cp ~/micro_ros_raspberrypi_pico_sdk/build/pico_micro_ros_example.uf2 /media/$USER/RPI-RP2
 
-# Step 4— Plug-in Pico to show up as USB drive and copy the uf2 file generated for the example code
-#
-# At this point hold the BOOTSEL button down on Pico and plugin the USB cable into your computer. It should show up as USB drive.
-#
-# Copy the .uf2 file generated from build step earlier to Pico like this
 
-# cp build/pico_micro_ros_example.uf2 /media/$USER/RPI-RP2

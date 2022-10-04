f611af309840b8070ad9805fcf81e2ec7abc0aa6
jarleven <jarleven@gmail.com>
2022-10-04 20:36:20 +0200

    Update setup.sh

--- Micro-ROS/setup.sh
+++ Micro-ROS/setup.sh
@@ -13,3 +13,13 @@ echo "export PICO_TOOLCHAIN_PATH=..." >> ~/.bashrc
 echo "export PICO_SDK_PATH=$HOME/micro_ros_ws/src/pico-sdk" >> ~/.bashrc
 source ~/.bashrc
 
+
+mkdir -p ~/micro_ros_ws/src
+cd ~/micro_ros_ws/src
+### clone the pico-sdk
+git clone --recurse-submodules https://github.com/raspberrypi/pico-sdk.git
+### clone the micro-ros example repo. This example simply publishes an incrementing integer value every second
+git clone https://github.com/micro-ROS/micro_ros_raspberrypi_pico_sdk.git
+
+
+
